4bae7e34df0344cb0f5ec229372440594f47e2e0
macoco-plan <macoco.plan@gmail.com>
2024-09-24 11:18:05 +0900

    新規提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+//　体重と身長のBMI
+console.log ( 68 / (1.7 * 1.7) );
+
+//　変数の宣言
+let weight;
+let height;
+let BMI;
+
+//　値の代入
+weight = 68;
+height = 1.7;
+BMI = (weight / (height*height));
+
+// コンソールへの出力
+console.log = BMI;
+
+

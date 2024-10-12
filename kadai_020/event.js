86027ef55299469a22ccba33e9e88d91da399e33
macoco-plan <macoco.plan@gmail.com>
2024-10-13 07:23:37 +0900

    再提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,9 +1,14 @@
-// textというidを持つHTML要素を取得し、定数に代入する
+// btnというidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
 
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
-    // 「ボタンをクリックしました」と表示を変更する
+
+    // 「ボタンをクリックしました」と表示する
     text.textContent = 'ボタンをクリックしました';
+
 })
 
